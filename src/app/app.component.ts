5c3a35021bb2a2d6c6e541774ef916dcc707fee5
GianLucaVitale000 <164628034+GianLucaVitale000@users.noreply.github.com>
2024-06-13 09:11:57 +0200

    modifiche 13-06-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { MenuModule } from 'primeng/menu';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'iot-dashboard';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-fw pi-home',
+        routerLink: ['/dashboard'],
+      },
+      {
+        label: 'Prodotti',
+        icon: 'pi pi-fw pi-shopping-cart',
+        routerLink: ['/prodotti'],
+      },
+    ];
+  }
 }
